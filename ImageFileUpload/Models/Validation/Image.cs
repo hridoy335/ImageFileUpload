1f688da9369fe93f12c35e4516337dbdf74ba1ab
Hridoy Roy <15103182@iubat.edu>
2020-08-30 15:38:03 +0600

    Add database with this project

---
+++ ImageFileUpload/Models/Validation/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ImageFileUpload.Models.Validation
+{
+    [MetadataType(typeof(MetadataImage))]
+    public partial class Image
+    {
+    }
+
+    public class MetadataImage
+    {
+        public byte[] ImageName { get; set; }
+        public System.DateTime MakeDate { get; set; }
+    }
+}
