b80e12b2581c738c7264fb5d5a216734630d7243
Hridoy Roy <15103182@iubat.edu>
2020-09-01 03:00:19 +0600

    Complete image insert and update

--- ImageFileUpload/Controllers/HomeController.cs
+++ ImageFileUpload/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Grpc.Core;
 using ImageFileUpload.Models;
 
 namespace ImageFileUpload.Controllers
 {
     public class HomeController : Controller
     {
-      ImageUploadEntities db = new ImageUploadEntities();
+        ImageUploadEntities db = new ImageUploadEntities();
         public ActionResult Index()
         {
             return View();
@@ -47,7 +50,7 @@ namespace ImageFileUpload.Controllers
             }
             else
             {
-            
+
                 image1.ImageName = path;
                 image1.MakeDate = System.DateTime.Now;
                 db.Images.Add(image1);
@@ -58,23 +61,62 @@ namespace ImageFileUpload.Controllers
             return View();
         }
 
+        public string EditImage(HttpPostedFileBase file) 
+        {
+            Random ran = new Random();
+            string path = "-1";
+            int random = ran.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extention = Path.GetExtension(file.FileName);
+                if (extention.ToLower().Equals(".jpg") || extention.ToUpper().Equals(".JPG") || extention.ToLower().Equals(".png"))
+                {
+                    try
+                    {
+                        path = Path.Combine(Server.MapPath("~/Content/upload/"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+
+                }
+                else
+                {
+                    Response.Write("<script>alrt('Only jpg or JPG or png formate'); <script>");
+                }
+
+            }
+            else
+            {
+                Response.Write("<script>alrt('Please select file ..'); <script>");
+                path = "-1";
+
+            }
+
+
+            return path;
+        }
+
         public string UploadImage(HttpPostedFileBase file)
         {
             Random ran = new Random();
             string path = "-1";
             int random = ran.Next();
-            if(file!=null && file.ContentLength > 0)
+            if (file != null && file.ContentLength > 0)
             {
                 string extention = Path.GetExtension(file.FileName);
-                if (extention.ToLower().Equals(".jpg")|| extention.ToUpper().Equals(".JPG")|| extention.ToLower().Equals(".png"))
+                if (extention.ToLower().Equals(".jpg") || extention.ToUpper().Equals(".JPG") || extention.ToLower().Equals(".png"))
                 {
                     try
                     {
-                        path = Path.Combine(Server.MapPath("~/Content/upload/"),random + Path.GetFileName(file.FileName));
+                        path = Path.Combine(Server.MapPath("~/Content/upload/"), random + Path.GetFileName(file.FileName));
                         file.SaveAs(path);
-                        path ="~/Content/upload/" + random + Path.GetFileName(file.FileName);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
                     }
-                    catch(Exception ex)
+                    catch (Exception ex)
                     {
                         path = "-1";
                     }
@@ -101,5 +143,69 @@ namespace ImageFileUpload.Controllers
         {
             return View(db.Images.ToList());
         }
+
+        [HttpGet]
+        public ActionResult ImageUpdate(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Images.Find(id);
+            //TempData["imgpath"] = image.ImageName;
+            if (image != null)
+            {
+                Session["imgpath"] = image.ImageName;
+            }
+            
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        [HttpPost]
+        public ActionResult ImageUpdate(Image image, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+
+                if (file != null)
+                {
+                    Image image1 = new Image();
+                    string path = EditImage(file);
+
+                    string Oldfilename = Request.MapPath(Session["imgpath"].ToString());
+                    image.ImageName = path;
+                    image.MakeDate = System.DateTime.Now;
+                    //db.Images.Add(image1);
+                    db.Entry(image).State = EntityState.Modified;
+                    db.SaveChanges();
+                    if (System.IO.File.Exists(Oldfilename))
+                    {
+                        System.IO.File.Delete(Oldfilename);
+
+                    }
+
+                    return RedirectToAction("ViewImage");
+
+                }
+                else
+                {
+                    Image df = new Image();
+                    image.ImageName = Session["imgpath"].ToString();
+                    //  MakeDate = Convert.ToDateTime(image.MakeDate);
+                    //image.MakeDate
+                    //image.ImageName=image.
+                    db.Entry(image).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("ViewImage");
+
+                }
+            return View();
+        }
     }
 }
+
+
+
